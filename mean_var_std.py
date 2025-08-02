744cdd5e98939ebf24b8097bf1b0a833da5d49d0
Daniel Tving <daniel.tving@gmail.com>
2025-08-02 13:39:31 +0200

    added input validation for list length in calculate() function

---
+++ mean_var_std.py
@@ -0,0 +1,9 @@
+def calculate(input_list):
+    """
+    Validates the input list for 9 numeric values.
+    Further statistical calculations will follow.
+    """
+    if len(input_list) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    # TODO: reshape into matrix and compute statistics
